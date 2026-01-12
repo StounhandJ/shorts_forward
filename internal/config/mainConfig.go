e40b5c740f1810fb55d00d5aa6cfa149b0e7a394
StounhandJ <rmarinicev@gmail.com>
2026-01-13 01:03:36 +0300

    fix(config): no required proxy-url

--- internal/config/mainConfig.go
+++ internal/config/mainConfig.go
@@ -7,5 +7,5 @@ type Config struct {
 type Application struct {
 	LogLevel   string `yaml:"LogLevel" env:"LOGLEVEL"`
 	TGBotToken string `yaml:"TGBotToken" env:"TG_BOT_TOKEN" flag:"tg-bot-token" usage:"Токен телегам бота"`
-	ProxyURL   string `yaml:"ProxyURL" env:"PROXY_URL" flag:"proxy-url" usage:"Прокси для отправки запросов"`
+	ProxyURL   string `yaml:"ProxyURL" env:"PROXY_URL" flag:"proxy-url" cli:"optional" usage:"Прокси для отправки запросов"`
 }
